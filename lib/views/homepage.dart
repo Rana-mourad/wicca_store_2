23f700b3d56a087584852aef9319b8da4023651d
Rana-mourad <105514841+Rana-mourad@users.noreply.github.com>
2023-11-26 07:32:54 +0200

    carousel

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -23,39 +25,120 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentPosition = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('wicca'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            onPageChanged: (index, _) {
+              currentPosition = index;
+              setState(() {});
+            },
+            height: 150,
+            aspectRatio: 16 / 9,
+            viewportFraction: 0.8,
+            initialPage: 0,
+            enableInfiniteScroll: true,
+            reverse: false,
+            autoPlay: true,
+            autoPlayInterval: Duration(seconds: 3),
+            autoPlayAnimationDuration: Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enlargeCenterPage: true,
+            enlargeFactor: 0.3,
+            scrollDirection: Axis.horizontal,
+          ),
+          items: [
             ProductCard(
-              title: 'childern suit',
-              description: 'nice suit for girle childern.',
-              price: 19.99,
+              title: 'Special Offer',
+              description: 'Limited time offer!',
+              price: 49.99,
               imageProvider: AssetImage('images/1.png'),
             ),
             ProductCard(
-              title: 'GenZ shirt',
-              description: 'awesome t-shirt for local brand.',
-              price: 29.99,
+              title: 'New Arrivals',
+              description: 'Explore our latest products.',
+              price: 39.99,
               imageProvider: AssetImage('images/2.png'),
             ),
             ProductCard(
-              title: 'red chose',
-              description: 'Famous brand.',
-              price: 39.99,
+              title: 'Best Sellers',
+              description: 'Discover our top-selling items.',
+              price: 59.99,
+              imageProvider: AssetImage('images/3.png'),
+            ),
+            ProductCard(
+              title: 'Fashion Collection',
+              description: 'Trendy styles for every occasion.',
+              price: 69.99,
+              imageProvider: AssetImage('images/1.png'),
+            ),
+            ProductCard(
+              title: 'Holiday Deals',
+              description: 'Celebrate with our special offers.',
+              price: 79.99,
               imageProvider: AssetImage('images/6.png'),
             ),
-          ],
+          ].map((product) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(color: Colors.amber),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image(
+                        image: product.imageProvider,
+                        height: 100,
+                        width: 100,
+                      ),
+                      SizedBox(height: 8),
+                      Text(product.description),
+                      SizedBox(height: 8),
+                      Text('Price: \$${product.price}'),
+                      SizedBox(height: 8),
+                      ElevatedButton(
+                        onPressed: () {
+                          context.read<CartProvider>().addToCart(
+                                product.title,
+                                product.price,
+                              );
+                        },
+                        child: Text('Add to Cart'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }).toList(),
         ),
-      ),
+        DotsIndicator(
+          dotsCount: 5,
+          position: currentPosition,
+          decorator: DotsDecorator(
+            size: const Size.square(9.0),
+            activeSize: const Size(18.0, 9.0),
+            activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
